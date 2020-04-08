b636a5cb33dfbc8538b2ec11d83546ba3d4efcaa
calvincurry22 <calvincurry22@gmail.com>
2020-04-08 10:44:01 -0500

    Added functionality to include departments in the employee cards

--- scripts/employees/EmployeeList.js
+++ scripts/employees/EmployeeList.js
@@ -1,6 +1,7 @@
 import { Employee } from "./Employee.js";
 import { useComputers } from "../computers/ComputerDataProvider.js";
 import { useEmployees } from "./EmployeeDataProvider.js";
+import { useDepartments } from "../departments/DepartmentDataProvider.js";
 
 const contentTarget = document.querySelector(".container")
 
@@ -8,11 +9,13 @@ const contentTarget = document.querySelector(".container")
 export const EmployeeList = () => {
     const employeeCollection = useEmployees()
     const computerCOllection = useComputers()
+    const departmentCollection = useDepartments()
 
     employeeCollection.map(singleEmployee => {
         const foundComputer = computerCOllection.find(singleComputer => singleComputer.id === singleEmployee.computerId)
+        const foundDepartment = departmentCollection.find(singleDepartment => singleEmployee.departmentId === singleDepartment.id)
 
-        contentTarget.innerHTML += Employee(singleEmployee, foundComputer)
+        contentTarget.innerHTML += Employee(singleEmployee, foundComputer, foundDepartment)
         
     }).join(" ")
 }
--- scripts/main.js
+++ scripts/main.js
@@ -1,9 +1,17 @@
 import { getEmployees } from "./employees/EmployeeDataProvider.js";
 import { getComputers } from "./computers/ComputerDataProvider.js";
 import { EmployeeList } from "./employees/EmployeeList.js";
+import { getDepartments } from "./departments/DepartmentDataProvider.js";
 
 
 
 getComputers()
     .then(getEmployees)
+    .then(getDepartments)
     .then(EmployeeList)
+
+
+
+
+
+    
